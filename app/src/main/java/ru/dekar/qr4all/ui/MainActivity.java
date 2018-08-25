2a6bfea2830e27927e8502184f4a09a725192c2c
Denis Karakoluichka <karakoluchka@kassir.ru>
2018-08-26 01:52:55 +0300

    activities

---
+++ app/src/main/java/ru/dekar/qr4all/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.dekar.qr4all.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.dekar.qr4all.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
